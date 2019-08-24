19ed831a1ce73db482f7c4e34a9b5148fad7e0aa
avishek835 <32808837+avishek835@users.noreply.github.com>
2019-08-24 15:33:47 +0530

    updated

--- FreeCRMUsingBDD/src/test/java/myRunner/TestRun.java
+++ FreeCRMUsingBDD/src/test/java/myRunner/TestRun.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
 		features = "/FreeCRMUsingBDD/src/test/java/featureFile/Login.feature", //the path of the feature files
 		glue={"/FreeCRMUsingBDD/src/test/java/stepDefinitions"}, //the path of the step definition files
-		format= {"pretty","html:test-outout", "json:json_output/cucumber.json", "junit:junit_xml/cucumber.xml"}, //to generate different types of reporting
+//		format= {"pretty","html:test-outout", "json:json_output/cucumber.json", "junit:junit_xml/cucumber.xml"}, //to generate different types of reporting
 		monochrome = true, //display the console output in a proper readable format
 		strict = true, //it will check if any step is not defined in step definition file
 		dryRun = false //to check the mapping is proper between feature file and step def file
